0f0d255533e7065c66c7b76728c1175485829907
Fingerlingus <sirfingerlingus@gmail.com>
2022-06-01 03:27:16 -0600

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,100 @@
 #![no_std]
 #![no_main]
+#![feature(linkage)]
+#![feature(exclusive_range_pattern)]
 
 use core::panic::PanicInfo;
 use core::arch::asm;
-use stivale_boot::v2::{StivaleFramebufferHeaderTag, StivaleHeader};
+use stivale_boot::v2::*;
 
-static COM1: u16 = 0x3F8;
-static STACK: [u8; 4096] = [0; 4096];
+mod constants;
+mod pager;
 
-static FRAMEBUFFER_TAG: StivaleFramebufferHeaderTag =
-    StivaleFramebufferHeaderTag::new().framebuffer_bpp(24);
+const COM1: u16 = 0x03F8;
+
+static INIT_STACK: [u8; 4096] = [0; 4096];
+static mut TERM_TAG: Option<&StivaleTerminalTag> = None;
+
+extern "C" {
+    #[linkage = "external"] static __stack_begin: *const ();
+    #[linkage = "external"] static __stack_end:   *const ();
+}
+
+static STIVALE_FRAMEBUFFER_TAG: StivaleFramebufferHeaderTag = StivaleFramebufferHeaderTag::new();
+/*
+static STIVALE_EFI_SYSTEM_TABLE_TAG: StivaleEfiSystemTableTag = StivaleEfiSystemTableTag::new()
+    .next((&STIVALE_FRAMEBUFFER_TAG as *const StivaleFramebufferHeaderTag).cast());
+
+static STIVALE_EPOCH_TAG: StivaleEpochTag = StivaleEpochTag::new()
+    .next((&STIVALE_EFI_SYSTEM_TABLE_TAG as *const StivaleEfiSystemTableTag).cast());
+
+static STIVALE_MEMORY_MAP_TAG: StivaleMemoryMapTag = StivaleMemoryMapTag::new()
+    .next((&STIVALE_EPOCH_TAG as *const StivaleEpochTag).cast());
+
+static STIVALE_RSDP_TAG: StivaleRsdpTag = StivaleRsdpTag::new()
+    .next((&STIVALE_MEMORY_MAP_TAG as *const StivaleMemoryMapTag).cast());
+
+static STIVALE_SMBIOS_TAG: StivaleSmbiosTag = StivaleSmbiosTag::new()
+    .next((&STIVALE_RSDP_TAG as *const StivaleRsdpTag).cast());
+*/
+static STIVALE_TERMINAL_TAG: StivaleTerminalHeaderTag = StivaleTerminalHeaderTag::new()
+    .next((&STIVALE_FRAMEBUFFER_TAG as *const StivaleFramebufferHeaderTag).cast());
 
 #[link_section = ".stivale2hdr"]
 #[no_mangle]
 #[used]
 static STIVALE_HDR: StivaleHeader = StivaleHeader::new()
-    .stack(&STACK[4095] as *const u8)
-    .tags((&FRAMEBUFFER_TAG as *const StivaleFramebufferHeaderTag).cast());
+    .stack(&INIT_STACK[4095] as *const u8)
+    .tags((&STIVALE_TERMINAL_TAG as *const StivaleTerminalHeaderTag).cast());    
 
-fn printstr_serial<const N: usize>(s: &[u8; N]) {
-    for &char in s.iter() {
+fn printstr_serial(s: &str) {
+    for &c in s.as_bytes().iter() {
         unsafe {
-            asm!("out dx, al", in("dx") COM1, in("al") char);
+            asm!("out dx, al", in("dx") COM1, in("al") c);
         }
     }
 }
 
+fn printstr_terminal(s: &str) {
+    unsafe {
+        match TERM_TAG {
+            Some(t) => t.term_write()(s),
+            None => { }
+        };
+    }
+}
+
+fn printstr(s: &str) {
+    printstr_terminal(s);
+    printstr_serial(s);
+}
+
 #[no_mangle]
-extern "C" fn entry_point(_header_addr: usize) -> ! {
-    printstr_serial(b"Hello, world!\n");
+extern "C" fn entry(boot_info: &'static StivaleStruct) -> ! {
+    unsafe { 
+        TERM_TAG = match boot_info.terminal() {
+            Some(t) => Some(t),
+            None => None
+        };
+
+        if __stack_end as usize - __stack_begin as usize <= 4096 {
+            panic("Stack is too small!");
+        } else {
+            printstr("Stack size is valid.\n");
+        }
+
+        let _p: pager::Pager;
+    }
+    loop {}
+}
+
+fn panic(s: &str) {
+    printstr(s);
     loop {}
 }
 
 #[panic_handler]
-fn panic(_infos: &PanicInfo) -> ! {
+fn panic_handler(_info: &PanicInfo) -> ! {
+    printstr("Panicking!");
     loop {}
 }
