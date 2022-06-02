e7f8bbc36433b0fb3937b92996cf466d54049be7
Fingerlingus <sirfingerlingus@gmail.com>
2022-06-01 21:46:33 -0600

    increase memory

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -2,7 +2,8 @@ qemu-system-x86_64 \
     -bios /usr/share/ovmf/x64/OVMF.fd \
     -drive id=disk,file=image.hdd,if=none,format=raw \
     -device ahci,id=ahci \
-    -device ide-hd,drive=disk,bus=ahci.0 -m 256M \
+    -device ide-hd,drive=disk,bus=ahci.0 \
+    -m 1G \
     -serial stdio \
     -no-reboot \
     -no-shutdown \
